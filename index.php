c47224765345c7188fabe51a72cfe67ae6f4fba3
vanrajsinh rathod <vanrajsinh2067@gmail.com>
2019-08-01 14:13:11 +0530

    New code of php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>My first program</title>
+</head>
+<h1>My First Visual Studio Code example<h1>
+
+<?php 
+    echo "My first example";
+
+    // using variable
+
+    $var = "hello, vanrajsinh rathod";
+    echo "$var";
+?>
+</html>
